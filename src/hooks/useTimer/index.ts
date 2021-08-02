15d835ec62e2ca3c388c1f8229094b1675528ada
slogup <slogup@slogupui-MacBookAir.local>
2021-08-03 01:07:23 +0900

    useTimer 로직 작성 완료

--- src/hooks/useTimer/index.ts
+++ src/hooks/useTimer/index.ts
@@ -1,13 +1,23 @@
+import {useEffect, useState} from "react";
+
 interface Time{
     min:number
     sec:number
 }
 
 export default function useTimer(onTimeEnd?:()=>void): Time{
-    const min = 0
-    const sec = 0
+    const [{min, sec}, setTime] = useState({min: 3, sec: 0});
 
+    useEffect(() => {
+        const timer: NodeJS.Timeout = setInterval(() => {
+            if (min === 0 && sec === 0) return clearInterval(timer);
+            if (min !== 0 && sec === 0) return setTime(prevTime => ({...prevTime, min: prevTime.min - 1, sec: 59}));
+            if (sec !== 0) return setTime(prevTime => ({...prevTime, sec: prevTime.sec - 1}));
+        }, 1000);
 
-    return { min, sec}
+        return () => clearInterval(timer);
+    }, [min, sec]);
+
+    return {min, sec}
 }
 
