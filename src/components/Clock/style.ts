ce95f30c7725cf271ee6728433a56ffa16b06d60
dabinim09 <dabinim09@gmail.com>
2021-08-03 18:07:36 +0900

    no message

--- src/components/Clock/style.ts
+++ src/components/Clock/style.ts
@@ -4,3 +4,4 @@ export const ClockTimeWrapper = styled.span<any>`
   font-weight: bold;
   font-size: ${({ theme }) => theme.fontSizes.large};
 `;
+
---
+++ src/components/Tutorial/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export interface Props {}
+
+const Tutorial: React.FC<Props> = () => {
+
+    return(
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
+
+export default Tutorial;
