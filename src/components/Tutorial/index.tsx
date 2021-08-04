f0d2cd86e96a050e85c1fd15413cdcf2d078164d
slogup <>
2021-08-04 14:07:19 +0900

    ing: 튜토리얼 페이지 시멘틱 html 작성 후 스타일 적용 전

--- src/components/Tutorial/index.tsx
+++ src/components/Tutorial/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import tutorialImage from '../../imgs/tutorial.png';
+
 export interface Props {}
 
 const Tutorial: React.FC<Props> = (props: Props) => {
     return (
-        <div>
-            튜토리얼 페이지
-        </div>
+        <>
+            <h1>모바일 정비명세서</h1>
+            <p>정비 받은 명세서를 쉽고 간편하게 모바일로 관리해보세요</p>
+            <img src={tutorialImage} alt="튜토리얼 이미지"/>
+            <button>
+                카펫 시작하기
+            </button>
+        </>
     );
 };
 
