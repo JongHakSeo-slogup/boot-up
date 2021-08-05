3f4486b15ace94cdc16df0d1a8538f47da300938
slogup <>
2021-08-05 10:18:07 +0900

    done: 튜토리얼 스킵 기능 완료 (로컬 스토리지에 저장된 값에 따라 로그인 페이지로 이동 확인)

--- src/components/Tutorial/index.tsx
+++ src/components/Tutorial/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
 import tutorialImage from '../../imgs/tutorial.png';
-
 import {Title, Description, ImageFigure, Image, ButtonContainer} from "./styles";
 import { BlockBlackBiggerButton } from '../Button/styles';
+
+import {useHistory} from "react-router";
+import {TUTORIAL_VIEWED} from "../../constants/tutorial";
+import URLS from "../../routes/urls";
+
 
 export interface Props {}
 
 const Tutorial: React.FC<Props> = (props: Props) => {
+    const history = useHistory();
+
+    const startCarpet = () => {
+        localStorage.setItem(TUTORIAL_VIEWED, 'true');
+        history.push(URLS.LOGIN_PAGE);
+    }
 
     return (
         <>
@@ -17,7 +27,7 @@ const Tutorial: React.FC<Props> = (props: Props) => {
                 <Image src={tutorialImage} alt="튜토리얼 이미지"/>
             </ImageFigure>
             <ButtonContainer>
-                <BlockBlackBiggerButton>
+                <BlockBlackBiggerButton onClick={startCarpet}>
                     카펫 시작하기
                 </BlockBlackBiggerButton>
             </ButtonContainer>
